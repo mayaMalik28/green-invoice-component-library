ead6ad433cbf2e9870064d51e19703f76c4dcff6
Maya Malik <maya28892@gmail.com>
2021-06-22 03:20:13 +0300

    try to pack

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import MaButton from "./components/Button/Button.vue";
-import MaTextField from './components/TextField/TextField.vue';
-import MaForm from './components/Form/Form.vue';
-import MaLoader from './components/Loader/Loader.vue';
+import MaButton from "@/components/Button/Button.vue";
+import MaTextField from '@/components/TextField/TextField.vue';
+import MaForm from '@/components/Form/Form.vue';
+import MaLoader from '@/components/Loader/Loader.vue';
 
 export {
     MaButton,
---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+import { VueConstructor } from "vue";
+
+export const MaButton: VueConstructor;
+export const MaTextField: VueConstructor;
+export const MaForm: VueConstructor;
+export const MaLoader: VueConstructor;
